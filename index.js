2c0cba57b9c854db4703d51810101973249b7ec0
Natalia Matat <n.matat@gmail.com>
2022-09-27 13:34:06 +0300

    basic functionality added

---
+++ index.js
@@ -0,0 +1,55 @@
+let cards = [];
+let sum = 0;
+let hasBlackJack = false;
+let isAlive = false;
+let message = '';
+let messageEl = document.getElementById('message-el');
+let sumEl = document.getElementById('sum-el');
+let cardEl = document.getElementById('cards-el');
+
+function getRandomCard() {
+	let randomNumber = Math.floor(Math.random() * 13) + 1;
+	if (randomNumber === 1) {
+		return 11;
+	} else if (randomNumber >= 11) {
+		return 10;
+	} else {
+		return randomNumber;
+	}
+}
+
+function startGame() {
+	isAlive = true;
+	let firstCard = getRandomCard();
+	let secondCard = getRandomCard();
+	cards = [firstCard, secondCard];
+	sum = firstCard + secondCard;
+	renderGame();
+}
+
+function renderGame() {
+	sumEl.textContent = `Sum: ${sum}`;
+	cardEl.textContent = 'Cards: ';
+
+	for (let i = 0; i < cards.length; i++) {
+		cardEl.textContent += cards[i] + ' ';
+	}
+	if (sum <= 20) {
+		message = 'Do you want to draw a new card?';
+	} else if (sum === 21) {
+		message = "Wohoo! You've got Blackjack!";
+		hasBlackJack = true;
+	} else {
+		message = "You're out of the game!";
+		isAlive = false;
+	}
+	messageEl.textContent = message;
+}
+
+function newCard() {
+	console.log('Drawing a new card from the deck');
+	let card = getRandomCard();
+	sum += card;
+	cards.push(card);
+	renderGame();
+}
